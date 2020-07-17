2ea8873f674b263b7459dfe4b0a664ffebee0d88
Kor-coconut <56989261+Kor-coconut@users.noreply.github.com>
2020-07-18 02:02:02 +0900

    Add files via upload

---
+++ BOJ/CPP/test.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+    int n, m;
+    int tmp;
+    cin >> n >> m;
+
+    tmp = n / m;
+        
+}
+
+int main(int argc, char* argv[]){
+    solve();
+}
