6b7f34c9da9215b0529ab76e966ac7647b3d977d
Kor-coconut <56989261+Kor-coconut@users.noreply.github.com>
2020-07-25 03:51:25 +0900

    Add files via upload

---
+++ BOJ/CPP/boj_1001.cpp
@@ -0,0 +1,15 @@
+// BOJ 1001
+// A-B
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+	int A, B;
+	cin >> A >> B;
+	cout << A - B;
+}
+
+int main(int argc, char* argv[]) {
+	solution();
+}
