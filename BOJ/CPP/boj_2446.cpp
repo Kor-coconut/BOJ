cd939347fea00d1f3591c3f714ba73db2d85660c
권정후 <56989261+Kor-coconut@users.noreply.github.com>
2020-09-20 12:36:29 +0900

    Add files via upload

---
+++ BOJ/CPP/boj_2446.cpp
@@ -0,0 +1,30 @@
+//BOJ 2446
+//별 찍기 - 9 
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < i; j++) {
+			cout << " ";
+		}
+		for (int j = N * 2 - 1; j > i * 2; j--) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	for (int i = 1; i < N; i++) {
+		for (int j = N - 1; j > i; j--) {
+			cout << " ";
+		}
+		for (int j = 0 ; j < i * 2 + 1; j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+}
