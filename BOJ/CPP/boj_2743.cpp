747cbe7a4250dbd72ec9545d05ecc3c8bea34195
권정후 <56989261+Kor-coconut@users.noreply.github.com>
2020-10-07 02:11:01 +0900

    Add files via upload

---
+++ BOJ/CPP/boj_2743.cpp
@@ -0,0 +1,16 @@
+//BOJ 2743
+//단어 길이 재기
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solution() {
+	string str;
+	cin >> str;
+
+	cout << str.length() << "\n";
+}
+
+int main(int argc, char* argv[]) {
+	solution();
+}
