60cb1df5e0ccfd3f11462e61ff9ee5aca0f192a8
권정후 <56989261+Kor-coconut@users.noreply.github.com>
2020-10-31 15:26:02 +0900

    Add files via upload

---
+++ BOJ/CPP/boj_3046.cpp
@@ -0,0 +1,15 @@
+// BOJ 3046
+// R2
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solution() {
+	int R1, S;
+	cin >> R1 >> S;
+	cout << 2 * S - R1 << "\n";
+}
+
+int main(int argc, char* argv[]) {
+	solution();
+}
