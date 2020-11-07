e39a223a625bfae2b96e7a3fa8ed766dda1afecd
권정후 <56989261+Kor-coconut@users.noreply.github.com>
2020-11-07 16:33:44 +0900

    Add files via upload

---
+++ BOJ/CPP/boj_5338.cpp
@@ -0,0 +1,13 @@
+//BOJ 5338
+//마이크로소프트 로고
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	cout << "       _.-;;-._" << "\n";
+	cout << "'-..-'|   ||   |" << "\n";
+	cout << "'-..-'|_.-;;-._|" << "\n";
+	cout << "'-..-'|   ||   |" << "\n";
+	cout << "'-..-'|_.-''-._|" << "\n";
+}
